7c1ddacefd9608bc52e0a14083caeac734f9d641
Aditya Raj <adistrim.dev@gmail.com>
2025-06-17 20:52:54 +0530

    signin middleware

---
+++ server/src/middlewares/signin.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import { UserSelect } from 'src/db/types';
+import {z} from 'zod';
+
+const signinInputSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8).max(100),
+});
+
+export const validateSigninInput = (input: UserSelect) => {
+  return signinInputSchema.safeParse(input);
+};
+
+export function validateSigninInputMiddleware(req: Request, res: Response, next: NextFunction) {
+  const validationResult = validateSigninInput(req.body);
+
+  if (!validationResult.success) {
+    res.status(400).json({ error: "Bad request" });
+    return
+  }
+
+  next();
+};
