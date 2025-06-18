3c763572595cbb936c6a27e9d45ca0b363a9935a
Aditya Raj <adistrim.dev@gmail.com>
2025-06-19 00:50:08 +0530

    cors config on server side

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const DB_URL = process.env.DB_URL!;
 export const SECRET = process.env.SECRET!;
+export const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN!;
