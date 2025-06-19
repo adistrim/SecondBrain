037568fa3bf49ffba8d49a84899e06987f00feb1
Aditya Raj <adistrim.dev@gmail.com>
2025-06-19 09:12:32 +0530

    added signup api hook

---
+++ app/src/api/signup.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { API_URL } from "../config";
+
+export interface SignupRequest {
+  email: string;
+  password: string;
+}
+
+export interface SignupResponse {
+  message?: string;
+  error?: string;
+  status?: number;
+}
+
+export async function signupApi(
+  requestBody: SignupRequest,
+): Promise<SignupResponse> {
+  try {
+    const response = await axios.post(`${API_URL}/auth/signup`, requestBody, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response) {
+      switch (error.response.status) {
+        case 409:
+          return { error: "Account already exists, please signin." };
+        case 400:
+          return { error: "Something went wrong, please try again." };
+      }
+    }
+
+    throw Error;
+  }
+}
