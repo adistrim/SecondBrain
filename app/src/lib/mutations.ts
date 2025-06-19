21dd16dade166b09c2e7869f45995ac069008612
Aditya Raj <adistrim.dev@gmail.com>
2025-06-19 09:12:43 +0530

    added signup mutation

--- app/src/lib/mutations.ts
+++ app/src/lib/mutations.ts
@@ -2,6 +2,8 @@ import { useNavigate } from "@tanstack/react-router";
 import { useMutation } from "@tanstack/react-query";
 import { signinApi, type SigninCredentials, type SigninResponse } from "@/api/singin";
 import { toast } from "sonner";
+import { signupApi, type SignupRequest, type SignupResponse } from "@/api/signup";
+import { setTimeout } from "timers";
 
 export function useSigninMutation() {
   const navigate = useNavigate();
@@ -20,4 +22,25 @@ export function useSigninMutation() {
       toast.error("Something went wrong, please try again.");
     },
   });
+}
+
+export function useSignupMutation() {
+  const navigate = useNavigate();
+  
+  return useMutation<SignupResponse, Error, SignupRequest, unknown>({
+    mutationFn: (requestBody: SignupRequest) => signupApi(requestBody),
+    onSuccess(response) {
+      if (response.status === 201) {
+        toast.success(response.message || "Signup successful");
+        setTimeout(() => {
+          navigate({ to: "/signin" });
+        }, 1000);
+      } else if (response.error) {
+        toast.error(response.error);
+      }
+    },
+    onError() {
+      toast.error("Something went wrong, please try again.");
+    },
+  })
 }
